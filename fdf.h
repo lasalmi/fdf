66e595ed22b847e45aa2f31717c995d044746114
Lauri Salmi <lasalmi@student.hive.fi>
2022-05-28 13:22:29 +0300

    Adding premiliminary header and author file

---
+++ fdf.h
@@ -0,0 +1,14 @@
+#ifndef FDF_H
+
+# define FDF_H
+# include <math.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "./mlx/mlx.h"
+
+#endif
